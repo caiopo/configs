672cd4a5e16b8a9435a4426c3deb7dc64dfcf820
caiopo <caiopoliveira@gmail.com>
2017-10-02 10:56:41 -0300

    Actually use bash

--- install.sh
+++ install.sh
@@ -1,15 +1,16 @@
 #! /bin/bash
+
 CONFIG_DIR=~/.config
 CODE_DIR=${CONFIG_DIR}/Code/User
 FISH_DIR=${CONFIG_DIR}/fish
 
 # vscode
-ln -s (realpath keybindings.json) $CODE_DIR/keybindings.json
-ln -s (realpath settings.json) $CODE_DIR/settings.json
+ln -s $(realpath keybindings.json) ${CODE_DIR}/keybindings.json
+ln -s $(realpath settings.json) ${CODE_DIR}/settings.json
 
 # git
-ln -s (realpath gitconfig) ~/.gitconfig
+ln -s $(realpath gitconfig) ~/.gitconfig
 
 # fish
-ln -s (realpath config.fish) $FISH_DIR/config.fish
-ln -s (realpath functions) $FISH_DIR/functions
+ln -s $(realpath config.fish) ${FISH_DIR}/config.fish
+ln -s $(realpath functions) ${FISH_DIR}/functions
